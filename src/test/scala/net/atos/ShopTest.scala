685803c6751b4c96875975b54465c8a7b8e60bc3
stuartmcvean <stuartmcvean@gmail.com>
2016-08-09 13:43:24 +0100

    Modified tests to match expected values from step 2

--- src/test/scala/net/atos/ShopTest.scala
+++ src/test/scala/net/atos/ShopTest.scala
@@ -10,13 +10,13 @@ class ShopTest extends FreeSpec {
   "The shop should" - {
     "return the correct price for a valid input where" - {
       "the input is 3 apples" in {
-        assert(Shop.purchase(List(apple, apple, apple)).contains("1.80"))
+        assert(Shop.purchase(List(apple, apple, apple)).contains("1.20"))
       }
       "the input is 2 apples and 2 oranges" in {
-        assert(Shop.purchase(List(apple, apple, orange, orange)).contains("1.70"))
+        assert(Shop.purchase(List(apple, apple, orange, orange)).contains("1.10"))
       }
       "the input is 3 oranges" in {
-        assert(Shop.purchase(List(orange, orange, orange)).contains("0.75"))
+        assert(Shop.purchase(List(orange, orange, orange)).contains("0.50"))
       }
       "the input is zero items" in {
         assert(Shop.purchase(Nil).contains("0.00"))
@@ -34,6 +34,31 @@ class ShopTest extends FreeSpec {
       }
       "there is a banana in the cart" in {
         assert(Shop.purchase(List(apple, orange, "Banana")).isEmpty)
+      }
+    }
+    "convert items to price where" - {
+      "item is an apple" in {
+        assert(Shop.convertItem(apple) == 60)
+      }
+      "item is a orange" in {
+        assert(Shop.convertItem(orange) == 25)
+      }
+      "item is neither an apple or and orange" in {
+        assert(Shop.convertItem("Banana") == 0)
+      }
+    }
+    "correctly calculate the price of a list of prices where" - {
+      "the list of items contain no 0's" in {
+        assert(Shop.calculatePrice(List(60, 25)).contains("0.85"))
+      }
+      "the sum is greater that 10 pounds" in {
+        assert(Shop.calculatePrice(List(1000, 500, 20)).contains("15.20"))
+      }
+      "the items contain a 0" in {
+        assert(Shop.calculatePrice(List(60, 25, 0)).isEmpty)
+      }
+      "the list of items is empty" in {
+        assert(Shop.calculatePrice(Nil).contains("0.00"))
       }
     }
   }
